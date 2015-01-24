57b2ddfcc97a1712a5ead3acf7ade26a21e452f5
Sudharshan Sreenivasan <ssreenivasan@apigee.com>
2015-01-24 16:44:04 +0530

    go fmt

--- agent/agent.go
+++ agent/agent.go
@@ -1,19 +1,17 @@
 package agent
 
 import (
+	"log"
 	"os"
 	"os/signal"
-	"log"
-	
+
 	"github.com/zeromq/gyre"
 )
 
-
 type Agent struct {
-	Conn *gyre.Gyre
+	Conn     *gyre.Gyre
 	Hostname string
 }
-
 
 func New() *Agent {
 	a := Agent{}
@@ -33,27 +31,27 @@ func (agent *Agent) Start() {
 
 	c := make(chan os.Signal, 1)
 	// Block until SIGs are given
-	signal.Notify(c, os.Interrupt, os.Kill)	
-	
+	signal.Notify(c, os.Interrupt, os.Kill)
+
 	err := agent.Conn.Start()
 	if err != nil {
 		log.Fatalf("Error when starting monk agent - %s\n", err.Error())
 	}
 	agent.Conn.Join("MONK_AGENT")
-	
+
 	log.Printf("Started monk agent %s at %s\n", agent.Conn.Name(), agent.Hostname)
-	
+
 	for {
 		select {
-		case e := <- agent.Conn.Events():
+		case e := <-agent.Conn.Events():
 			switch e.Type() {
 			case gyre.EventShout:
 				log.Printf("Executing state - %s\n", string(e.Msg()))
-			}			
+			}
 		case s := <-c:
 			log.Printf("Got %s. Bye!\n", s)
 			os.Exit(0)
 		}
 	}
-	
+
 }
--- main.go
+++ main.go
@@ -1,45 +1,44 @@
 package main
 
 import (
-	"os"
 	"log"
+	"os"
 
-	_ "github.com/zeromq/gyre"
 	"github.com/codegangsta/cli"
+
+	"github.com/sudharsh/monk/agent"
 )
 
 func gatherCommands() []cli.Command {
-	return []cli.Command {
+	return []cli.Command{
 		{
-			Name: "agent",
+			Name:  "agent",
 			Usage: "Use this node as a monk agent",
-      		Flags: []cli.Flag {
-				cli.StringFlag {
-					Name: "master",
-					Usage: "Location of the master monk node",
+			Flags: []cli.Flag{
+				cli.StringFlag{
+					Name:   "master",
+					Usage:  "Location of the master monk node",
 					EnvVar: "MONK_MASTER",
 				},
 			},
 			Action: startAgent,
 		},
 		{
-			Name: "master",
-			Usage: "Use this node as a monk master",
+			Name:   "master",
+			Usage:  "Use this node as a monk master",
 			Action: startMaster,
 		},
 	}
 }
 
-
 func startMaster(c *cli.Context) {
 	log.Printf("Starting Master\n")
 }
 
-
 func startAgent(c *cli.Context) {
-	log.Printf("Starting agent\n")
+	ag := agent.New()
+	ag.Start()
 }
-
 
 func main() {
 	app := cli.NewApp()
